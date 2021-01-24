2f6b2326e34c12de78caadc475c1462d178de148
Manju410 <manjunathgbsn1994@gmail.com>
2021-01-24 12:59:28 +0530

    Print First5 Captains Names

---
+++ First5.py
@@ -0,0 +1,19 @@
+import re
+
+def FunctionToGetCaptainsName(SplitCaptain):
+    count=0
+    CaptainName=[]
+    for name in SplitCaptain:
+        if count==5:
+            break
+        matchObj = re.match("^Player", name)
+        if not matchObj:
+            splitname= name.split(',')
+            CaptainName.append(splitname[0])
+            count+=1
+    return CaptainName
+    
+Captains = open('C:\Manjunath\Personal\Manju\pi\Captains.txt', 'r').read()
+SplitCaptain = Captains.split('\n')
+
+print(FunctionToGetCaptainsName(SplitCaptain))
