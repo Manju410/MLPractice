5393b717d64a09c98e4fdd438ce7c935477865f1
Manju410 <manjunathgbsn1994@gmail.com>
2021-01-29 19:56:41 +0530

    Update PandasPython.py

--- PandasPython.py
+++ PandasPython.py
@@ -28,3 +28,42 @@ df[df["temprature"]==df["temprature"].max()] # printing rows having max temp
 df.index                        # to find index of dataframe
 df.set_index("day",inplace=True)  # setting index values to day column of dataframe
 df.reset_index(inplace=True)     # undoing dataframe column for reseting
+
+
+# Handling missing values
+
+#importing stocks table contains ticker, eps, revenue,price columns
+# prive header is none to skip headers, provide customize names for columns
+#dfexcel= pd.read_csv("C:\Manjunath\Personal\Manju\Pandas\csv\excel.csv",header=None, names=["ticker","eps","revenue","price","people"])
+
+#handle null values for particular words
+#dfexcel= pd.read_csv("C:\Manjunath\Personal\Manju\Pandas\csv\excel.csv",na_values=["not available","n.a"])
+
+#handle null values by using dictionary
+dfexcel= pd.read_csv("C:\Manjunath\Personal\Manju\Pandas\csv\excel.csv",na_values={
+    "eps":["not available","n.a"],
+    "revenue":["not available","n.a",-1],
+     "people":["not available","n.a",-1],
+     "price":["not available","n.a"]
+})
+
+
+#dfexcel.to_csv("C:\Manjunath\Personal\Manju\Pandas\csv\new.csv", index=false) # to write csv
+#dfexcel.to_csv("new.csv", columns=["tickcer","eps"]) # specified columns
+#dfexcel.to_csv("new.csv", header=false) # skiping header
+
+# Writing customize function to handle null values
+def convertpeople(cell):
+    if cell=="n.a":
+        return "sam walton"
+    return cell
+
+# call customize function by parameter converters
+dfex= pd.read_excel("C:\Manjunath\Personal\Manju\Pandas\excel.xlsx","Sheet1", converters={
+    "people":convertpeople
+})
+
+# exporting dataframe to excel sheet by making index false
+pd.to_excel("new.xlsx",sheet_name="stocks", index=False)
+
+pd.to_excel("new.xlsx",sheet_name="stocks", index=False,startrow=3,startcol=2) # writing from particular row and col
